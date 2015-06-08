f16716795954e81acc21f7bde2bb6e81bed3ca0d
Keith Gordon <keithg07@gmail.com>
2015-06-08 15:57:14 -0500

    refactored css stylesheet

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
+
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
@@ -8,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( users.css , font_styles.css )
+Rails.application.config.assets.precompile += %w( users.css , font_styles.css , login.css , signup.css )
