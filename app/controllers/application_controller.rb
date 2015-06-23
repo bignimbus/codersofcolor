6464dd8174dc93893b45d9514676c24aa8c7d62f
Keith Gordon <keithg07@gmail.com>
2015-06-23 18:21:27 -0500

    fixed params issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,12 +9,10 @@ class ApplicationController < ActionController::Base
   # end
 
       protected
-
-       def configure_permitted_parameters
-           devise_parameter_sanitizer.for(:sign_up) { |u| u.permit( :first_name, :last_name, :email, :password, :password_confirmation) }
-           devise_parameter_sanitizer.for(:account_update) << :first_name << :last_name << :bio << :website << :field
-
-       end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :first_name << :last_name <<:email << :password << :password_confirmation
+    devise_parameter_sanitizer.for(:account_update) << :first_name << :last_name << :bio << :website << :field
+  end
 
 
 end
