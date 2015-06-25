9b870a433a6cb40b95c74b168b41b43063d9d725
Keith Gordon <keithg07@gmail.com>
2015-06-24 21:03:23 -0500

    fixed sign_up & edit profile issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -8,9 +8,9 @@ class ApplicationController < ActionController::Base
   #   edit_user_registration_path
   # end
 
-      protected
+  protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :first_name << :last_name << :email << :password << :password_confirmation
+    devise_parameter_sanitizer.for(:sign_up) << :first_name << :last_name 
     devise_parameter_sanitizer.for(:account_update) << :first_name << :last_name << :bio << :website << :field
   end
 
