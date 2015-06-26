a7866ca02b70df49d2b18cd0cdaff80568cf94f6
Keith Gordon <keithg07@gmail.com>
2015-06-26 02:58:38 -0500

    changed user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates_uniqueness_of :email
-  validates_presence_of :first_name, :last_name
-  validates_presence_of :email
-
 
   def name
   [first_name, last_name].compact.join(' ')
