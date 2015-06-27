de4922a1e9e32f2099921048869b0191d7437a1b
Keith Gordon <keithg07@gmail.com>
2015-06-27 05:44:10 -0500

    changed authenication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,12 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-
- #  def show
- #    @user = User.find(params[:id])
- #  end
-
+  def show
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to :back, :alert => "Access denied."
+    end
+  end
  #  def new
  #    @user = User.new
  #  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'users#index'
-  resources :users
   devise_for :users
+  resources :users
 end
