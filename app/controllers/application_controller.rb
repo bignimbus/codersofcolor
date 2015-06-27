de2fd144352fedc57fea00e8c6c68ddee1c276a3
Keith Gordon <keithg07@gmail.com>
2015-06-27 04:23:08 -0500

    chagned application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
-  def after_sign_in_path_for(resource)
-    edit_user_registration_path
-  end
+  # def after_sign_in_path_for(resource)
+  #   edit_user_registration_path
+  # end
 
-  protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation ) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
