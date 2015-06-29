d8fe57ddc0b0029b93b17bcabd286a80948be7d4
Keith Gordon <keithg07@gmail.com>
2015-06-29 12:20:49 -0500

    removed invite code & add visitor controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
 
  def create
-   invite_code = params[:invite_code]
-   if invite_code == 'linkedhearts'
+  #  invite_code = params[:invite_code]
+  #  if invite_code == 'linkedhearts'
      super
-   else
-    redirect_to root_path, :notice => 'Your invitation code is not valid'
-    end
+  #  else
+    # redirect_to new_user_registration_path, :notice => 'Your invitation code is not valid'
+    # end
    end
 
  def new
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( users.css , font_styles.css , login.css , signup.css )
+Rails.application.config.assets.precompile += %w( login.css , signup.css , visitor.css )
