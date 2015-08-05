534e3574c1c40ed818015be4151b0a13db9b5f37
Keith Gordon <keithg07@gmail.com>
2015-08-05 11:25:22 -0500

    configured user model for role based authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
-class User < ActiveRecord::Base  
+class User < ActiveRecord::Base
+  enum role: [:basic,:plus,:premium,:admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :basic
+  end
+
 # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150801224128) do
+ActiveRecord::Schema.define(version: 20150805153417) do
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -30,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150801224128) do
     t.string   "website"
     t.string   "field"
     t.string   "bio"
+    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
