8d996ce473207c4cdda348749ff57bb8b3e2571f
Keith Gordon <keithg07@gmail.com>
2015-09-24 12:55:53 -0500

    edit devise permitted paramters

--- config/initializers/devise_premitted_paramaters.rb
+++ config/initializers/devise_premitted_paramaters.rb
@@ -9,7 +9,7 @@ module DevisePermittedParameters
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :first_name << :last_name << :role << :avatar
-    devise_parameter_sanitizer.for(:account_update) << :first_name << :last_name << :bio << :website << :field << :role << :avatar
+    devise_parameter_sanitizer.for(:account_update) << :first_name << :last_name << :bio << :website << :field << :role << :avatar << :twitter
   end
 
 end
