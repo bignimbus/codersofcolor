c3de6856c73c53f1290eff8b5e5c94435ae27f60
keithgordon <keithg07@gmail.com>
2016-10-08 10:05:10 -0500

    added about static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
   resources :dashboard
+  get '/about' => 'visitors#about'
   devise_for :users, controllers: { :registrations => 'registrations' }
   resources :users
 end
